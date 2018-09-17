1ba0aa8d25ddc0ec5a30df5dedf412bd79f4a0a7
Markus Tuomi <MarkusTuomi@Markuss-MBP-2.fios-router.home>
2018-09-17 13:03:47 -0400

    Add stuff to game

--- heart/src/org/tuomilabs/heart/heart/Game.java
+++ heart/src/org/tuomilabs/heart/heart/Game.java
@@ -1,7 +1,34 @@
-package org.tuomilabs.heart.heart;
+package org.tuomilabs.heart.core;
+import java.util.ArrayList;
 
 public class Game {
-    public static void main(String[] args) {
-
+	
+	public static void main(String[] args){
+		ArrayList<Card> hand = new ArrayList<Card>();
+		ArrayList<Card> trickcards = new ArrayList<Card>();
     }
+	
+	private void trick(){
+		
+	}
+	
+	private int trickwinner(ArrayList<Card> trickcards){
+		Card largest = new Card(trickcards.get(0));
+		for(Card i:trickcards){
+			if( i.getsuit() == largest.getsuit()){
+				if(i.getvalue() > largest.getvalue()){
+					largest.setvalue(i.getvalue());
+					largest.setplayer(i.getplayer());
+				}
+			}
+		}
+		return largest.getplayer();
+	}
+	
+	private boolean checkifstarting(ArrayList<Card> hand){
+		for(int i = 0; i < 13; i++){
+			if(hand.get(i).getvalue() == 0 && hand.get(i).getsuit() == 0) return true;
+		}
+		return false;
+	}
 }
