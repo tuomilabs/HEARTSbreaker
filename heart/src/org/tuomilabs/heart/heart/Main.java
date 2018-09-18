c0ce13cef998494664691aab8ace37b63eda5afe
AG8 <andrew2000g@gmail.com>
2018-09-18 00:23:24 -0400

    Cool algorithm

--- heart/src/org/tuomilabs/heart/heart/Main.java
+++ heart/src/org/tuomilabs/heart/heart/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Let's go!!");
 
-        new Main().train();
+        new Main().play();
     }
 
     private void train() throws Exception {
@@ -20,7 +20,7 @@ public class Main {
     private void play() throws Exception {
         // Initialize the GameSimulator
 
-        Double[] coefficientsArray = new Double[]{105.0, 0.6491194621242463, 1.752940358463908, 0.0, 0.7914513290561169, 62.0, 0.1136791236788397, 0.49844380209689865, 1.8478553796273784, 46.59543520494638, 161.6416210758341};
+        Double[] coefficientsArray = new Double[]{0.07121923642709405, 0.04780899399534995, 0.06708895987395794, 0.07936939055868675, 0.015890456003451556, 3.966960840201052, 4.60695094706519};
         List<Double> coefficients = Arrays.asList(coefficientsArray);
 
         List<List<Double>> four = new ArrayList<>();
